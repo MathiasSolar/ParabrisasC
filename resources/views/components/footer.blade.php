0dfe608dace6cecf135e67d07bc8ee1018694fa4
Mathias <mathiassolar24@gmail.com>
2023-08-19 17:13:09 -0400

    css footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,20 @@
-<footer>
-    Horarios
+<footer class="footer">
+    <section class="footer_image">
+        <div class="footer_image-item">
+            <img src="{{asset('img/Footer/autofooter.png')}}" alt="Auto">
+        </div>
+    </section>
+
+    <section class="footer_schedule">
+        <div class="footer_schedule-itemn">
+            <span class=title>Horarios</span>
+        </div>
+        <div>
+            <span class="normal-text">De lunes a viernes de 9:30 a 13:00 - 15:00 a 18:00</span>
+        </div>
+        <div>
+            <span class="normal-text"> Sabado de 10:00 a 13:00</span>
+        </div>
+    </section>
+
 </footer>

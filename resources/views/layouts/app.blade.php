eb5efb38c9048a10bab6cde3f4e7b3b9682ccc14
Ikeda Harket <sebaaignacio111@gmail.com>
2023-08-19 14:28:11 -0400

    Layout  * Se crean componentes de footer y header  * Se crea el layout principal de la web  * Se renderiza todo a traves del welcome

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>@yield('title')</title>
+    </head>
+    <body>
+        @include('components.header')
+        <main>
+            @yield('content')
+        </main>
+        @include('components.footer')
+    </body>
+</html>

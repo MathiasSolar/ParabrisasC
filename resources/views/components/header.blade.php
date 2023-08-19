25446257ef4a9491a230d550d035a53829091639
Ikeda Harket <sebaaignacio111@gmail.com>
2023-08-19 16:31:56 -0400

    ikeda

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,20 +2,20 @@
     <section class="header__contact">
         <div class="header__contact-item">
             <img src="{{asset('img/Header/whatsapp.png')}}" alt="Whatsapp">
-            <span>+56 9 57851763</span>
+            <span class="normal-text">+56 9 57851763</span>
         </div>
         <div class="header__contact-item">
             <img src="{{asset('img/Header/Telefono.png')}}" alt="Phone">
-            <span>67-2529002</span>
+            <span class="normal-text">67-2529002</span>
         </div>
         <div class="header__contact-item">
             <img src="{{asset('img/Header/Email.png')}}" alt="Email">
-            <span>alvaroarteaga23@yahoo.es</span>
+            <span class="normal-text">alvaroarteaga23@yahoo.es</span>
         </div>
     </section>
     <section class="header__main">
         <div class="header__logo">
-            <img src="" alt="Logo">
+            <img src="{{asset('img/Header/AutoAzul.png')}}" alt="Logo">
             <h1>Coyahique Parabrisas</h1>
         </div>
         <nav class="header__nav">

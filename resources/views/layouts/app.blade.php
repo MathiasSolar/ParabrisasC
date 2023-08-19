31c6d3b119d301698a7242cf25cad56f62aedde5
Ikeda Harket <sebaaignacio111@gmail.com>
2023-08-19 15:12:54 -0400

    Estilos conectados al layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>@yield('title')</title>
+        <link rel="stylesheet" href="{{asset('css/main.css')}}">
     </head>
     <body>
         @include('components.header')

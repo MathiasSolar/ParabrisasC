0ac2ea9d2b7b53a7546fb3e73f7c71e05f829ecb
Ikeda Harket <sebaaignacio111@gmail.com>
2023-08-19 15:54:29 -0400

    estructura inicial css

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,3 +1,29 @@
-<header>
-    header
+<header class="header">
+    <section class="header__contact">
+        <div class="header__contact-item">
+            <img src="" alt="Whatsapp">
+            <span>+56 9 57851763</span>
+        </div>
+        <div class="header__contact-item">
+            <img src="" alt="Phone">
+            <span>67-2529002</span>
+        </div>
+        <div class="header__contact-item">
+            <img src="" alt="Email">
+            <span>alvaroarteaga23@yahoo.es</span>
+        </div>
+    </section>
+    <section class="header__main">
+        <div class="header__logo">
+            <img src="" alt="Logo">
+            <h1>Coyahique Parabrisas</h1>
+        </div>
+        <nav class="header__nav">
+            <a href="#">Nosotros</a>
+            <a href="#">Servicios</a>
+            <a href="#">Ubicacion</a>
+            <a href="#">Galeria</a>
+            <a href="#">Horarios</a>
+        </nav>
+    </section>
 </header>

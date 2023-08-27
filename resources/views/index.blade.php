76dfd7ff99ff7e7dd67261c9642399ad48f993ad
Mathias <mathiassolar24@gmail.com>
2023-08-27 18:25:44 -0400

    brands

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,4 +10,5 @@
             En Coyhaique Parabrisas, nos enorgullece ser su destino confiable para todas sus necesidades de reparación y reemplazo de ventanas de automóviles. Con años de experiencia en el sector, nos hemos convertido en un referente en la región cuando se trata de mantener la seguridad y la estética de su vehículo en perfectas condiciones.
         </p>
     </section>
+    @include('components.brands')
 @endsection

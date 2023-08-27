6d87808b05839f2db70a1c684a5f4ffba88dfbc4
IkedaHarket <sebaaignacio111@gmail.com>
2023-08-27 19:27:14 -0400

    nuestros servicios

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,5 +10,52 @@
             En Coyhaique Parabrisas, nos enorgullece ser su destino confiable para todas sus necesidades de reparación y reemplazo de ventanas de automóviles. Con años de experiencia en el sector, nos hemos convertido en un referente en la región cuando se trata de mantener la seguridad y la estética de su vehículo en perfectas condiciones.
         </p>
     </section>
+    <section class="index-page__services">
+        <h4 class="title">Nuestros Servicios</h4>
+        <article class="services__item">
+            <picture>
+                <img src="{{asset('img/Services/Parabrisa.png')}}" alt="Parabrisas">
+            </picture>
+            <div class="services__item-info">
+                <h5 class="subtitle">Parabrisas</h5>
+                <p class="normal-text" >
+                    "La claridad y resistencia que tu vehículo merece. Con nuestros parabrisas, no solo garantizas una visión perfecta en la carretera, sino que también inviertes en seguridad y durabilidad. Es más que un simple vidrio, es una promesa de calidad y protección para tus viajes." 
+                </p>
+            </div>
+        </article>
+        <article class="services__item">
+            <picture>
+                <img src="{{asset('img/Services/Puerta.png')}}" alt="Vidrio de puerta">
+            </picture>
+            <div class="services__item-info">
+                <h5 class="subtitle" >Vidrios de puertas</h5>
+                <p class="normal-text" >
+                    "Una combinación de diseño y fortaleza. Nuestros vidrios para puertas no solo embellecen tu vehículo, sino que también ofrecen la máxima protección y resistencia en cada trayecto. Elegancia y seguridad de la mano."
+                </p>
+            </div>
+        </article>
+        <article class="services__item">
+            <picture>
+                <img src="{{asset('img/Services/Luneta.png')}}" alt="Lunetas">
+            </picture>
+            <div class="services__item-info">
+                <h5 class="subtitle" >Lunetas</h5>
+                <p class="normal-text" >
+                    "Visión trasera sin compromisos. Nuestras lunetas combinan transparencia de alta definición con una durabilidad insuperable, asegurando que tengas una perspectiva clara y segura en todo momento. Estilo y confianza en cada mirada hacia atrás."
+                </p>
+            </div>
+        </article>
+        <article class="services__item">
+            <picture>
+                <img src="{{asset('img/Services/lateral.png')}}" alt="Vidrios laterales">
+            </picture>
+            <div class="services__item-info">
+                <h5 class="subtitle" >Vidrios laterales</h5>
+                <p class="normal-text" >
+                    "Miradas al horizonte con la máxima protección. Nuestros vidrios laterales ofrecen una claridad impecable, mientras garantizan seguridad y resistencia contra los elementos. Viaja con la tranquilidad de estar rodeado de la mejor calidad."
+                </p>
+            </div>
+        </article>
+    </section>
     @include('components.brands')
 @endsection
